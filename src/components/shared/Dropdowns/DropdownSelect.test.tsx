2e76e36d49199f2a7627a2afb7d23d4d365f714a
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-04-05 22:41:30 +0100

    Add a few initial test cases

---
+++ src/components/shared/Dropdowns/DropdownSelect.test.tsx
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+import DropdownSelect from "./DropdownSelect";
+
+describe("the dropdown contains", () => {
+    test.todo("the correct items");
+    test.todo("no incorrect items");
+});
+
+describe("the dropdown functions", () => {
+    test.todo("correctly when selecting an option");
+});
