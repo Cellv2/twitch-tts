b5a0340dfedc264d0e179e147c4c353d89c00b37
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-04-05 22:31:16 +0100

    Add dropdown component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "./app.scss";
+import DropdownSelect from "./components/shared/Dropdowns/DropdownSelect";
 
 type AppProps = {
     title: string;
@@ -11,6 +12,7 @@ const App = ({ title }: AppProps) => (
         <main className="dark:bg-blue-950 dark:text-white min-h-dvh">
             <h1>{title}</h1>
             <p>Test App</p>
+            <DropdownSelect dropdownOptions={["One", "Two", "Three"]} />
         </main>
     </>
 );
